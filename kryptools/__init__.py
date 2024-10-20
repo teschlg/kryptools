921a370376a8f7e43b22a0d8331288ddd62f8f53
Gerald Teschl <gerald.teschl@univie.ac.at>
2024-10-20 11:11:03 +0200

    Cleanups in the primes module

--- kryptools/__init__.py
+++ kryptools/__init__.py
@@ -3,7 +3,7 @@ Implemenation of same basic algorithms used in cryptography.
 """
 
 from .nt import egcd, cf, convergents, legendre_symbol, jacobi_symbol, sqrt_mod, euler_phi, carmichael_lambda, moebius_mu, order, crt
-from .primes import sieve_eratosthenes, prime_pi, is_prime, next_prime, previous_prime, random_prime, random_strongprime, is_safeprime, random_safeprime
+from .primes import sieve_eratosthenes, prime_pi, is_prime, next_prime, previous_prime, random_prime, random_strongprime, is_safeprime, random_safeprime, is_blumprime, random_blumprime, miller_rabin_test, lucas_test
 from .factor import factorint, divisors
 from .dlp import dlog
 from .ec import EC_Weierstrass
--- kryptools/primes.py
+++ kryptools/primes.py
@@ -12,8 +12,9 @@ Tools for prime numbers:
     is_blumprime(n) test if n is a Blum prime
     random_blumprime(l) find a pseudorandom Blum prime with bit length at least l
     miller_rabin_test(n, b) Miller-Rabin primality test with base b
+    lucas_test(n) strong Lucas test
 """
-from math import isqrt, gcd, floor
+from math import isqrt, gcd, floor, ceil
 from random import randint
 from .nt import jacobi_symbol
 
@@ -71,75 +72,6 @@ def miller_rabin_test(n: int, bases: list[int] | int) -> bool:
                 return True
     return False
 
-def is_prime(n: int) -> bool:
-    """Test if an integer `n` if probable prime."""
-    if n < 18446744073709551616:  # https://miller-rabin.appspot.com
-        return miller_rabin_test(n, [2, 325, 9375, 28178, 450775, 9780504, 1795265022])
-    if n < 3317044064679887385961981:
-        return miller_rabin_test(n, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41])
-    return miller_rabin_test(n, [2]) and _is_strong_lucas_prp(n)  # Baillie–PSW primality test
-
-def next_prime(n: int) -> int:
-    """Find the next prime larger or equal `n`."""
-    if n < 3:
-        return 2
-    n |= 1  # make sure n is odd
-    while not is_prime(n):
-        n += 2
-    return n
-
-def previous_prime(n: int) -> int:
-    """Find the previous prime smaller or equal `n`."""
-    if n < 3:
-        return 2
-    n += (n & 1) - 1  # make sure n is odd
-    while not is_prime(n):
-        n -= 2
-    return n
-
-def random_prime(l: int) -> int:
-    """Find a pseudorandom prime with bit length at least `l`."""
-    return next_prime(randint(2 ** (l - 1), 2**l - 1))
-
-def random_strongprime(l: int) -> int:
-    """Find a pseudorandom strong prime with bit length at least `l` using Gordon's algorithm."""
-    t = random_prime(l)
-    s = random_prime(l)
-    u = 2 * t
-    uu = u * randint(1, 100)
-    while not is_prime(uu + 1):
-        uu += u
-    r = uu + 1
-    u = 2 * r * s
-    uu = u * randint(1, 100) + 2 * s * pow(s, r - 2, r) - 1
-    while not is_prime(uu):
-        uu += u
-    return t, s, r, uu
-
-def is_safeprime(p: int) -> bool:
-    """Tests if a number is a safe prime."""
-    return is_prime(p) and is_prime((p - 1) // 2)
-
-def random_safeprime(l: int) -> int:
-    """Find a pseudorandom safe prime with bit length at least `l` and `ord(2)=(p-1)/2`."""
-    p = randint(2 ** (l - 1), 2**l - 1)
-    p = p - (p % 24) + 23
-    while not is_safeprime(p):
-        p += 24
-    return p
-
-def is_blumprime(p: int) -> bool:
-    """Tests if a number is a Blum prime."""
-    return p % 4 == 3 and is_prime(p)
-
-def random_blumprime(l: int) -> int:
-    """Find a pseudorandom Blum prime with bit length at least `l`."""
-    p = randint(2 ** (l - 1), 2**l - 1)
-    p = p - (p % 4) + 3
-    while not is_prime(p):
-        p += 4
-    return p
-
 def _lucas_sequence(n, D, k):
     """Evaluate a Lucas sequence."""
     # P = 1
@@ -165,8 +97,8 @@ def _lucas_sequence(n, D, k):
     return U % n, V % n, Qk
 
 
-def _is_strong_lucas_prp(n: int) -> bool:
-    """Strong Lucas primality test."""
+def lucas_test(n: int) -> bool:
+    """Run a strong Lucas primality test on `n`."""
 
     # remove powers of 2 from n+1 (= k * 2**s)
     k = (n + 1) // 2
@@ -200,3 +132,78 @@ def _is_strong_lucas_prp(n: int) -> bool:
             return True
         Qk = pow(Qk, 2, n)
     return False
+
+def is_prime(n: int) -> bool:
+    """Test if an integer `n` if probable prime."""
+    if n < 18446744073709551616:  # https://miller-rabin.appspot.com
+        return miller_rabin_test(n, [2, 325, 9375, 28178, 450775, 9780504, 1795265022])
+    if n < 3317044064679887385961981:
+        return miller_rabin_test(n, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41])
+    return miller_rabin_test(n, [2]) and lucas_test(n)  # Baillie–PSW primality test
+
+def is_safeprime(p: int) -> bool:
+    """Tests if a number is a safe prime."""
+    return is_prime(p) and is_prime((p - 1) // 2)
+
+def is_blumprime(p: int) -> bool:
+    """Tests if a number is a Blum prime."""
+    return p % 4 == 3 and is_prime(p)
+
+
+# Generation of primes
+
+
+def next_prime(n: int) -> int:
+    """Find the next prime larger or equal `n`."""
+    if n < 3:
+        return 2
+    n = ceil(n)
+    n |= 1  # make sure n is odd
+    while not is_prime(n):
+        n += 2
+    return n
+
+def previous_prime(n: int) -> int:
+    """Find the previous prime smaller or equal `n`."""
+    if n < 3:
+        return 2
+    n = floor(n)
+    n += (n & 1) - 1  # make sure n is odd
+    while not is_prime(n):
+        n -= 2
+    return n
+
+def random_prime(l: int) -> int:
+    """Find a pseudorandom prime with bit length at least `l`."""
+    return next_prime(randint(2 ** (l - 1), 2**l - 1))
+
+def random_strongprime(l: int) -> int:
+    """Find a pseudorandom strong prime with bit length at least `l` using Gordon's algorithm."""
+    t = random_prime(l)
+    s = random_prime(l)
+    u = 2 * t
+    uu = u * randint(1, 100)
+    while not is_prime(uu + 1):
+        uu += u
+    r = uu + 1
+    u = 2 * r * s
+    uu = u * randint(1, 100) + 2 * s * pow(s, r - 2, r) - 1
+    while not is_prime(uu):
+        uu += u
+    return t, s, r, uu
+
+def random_safeprime(l: int) -> int:
+    """Find a pseudorandom safe prime with bit length at least `l` and `ord(2)=(p-1)/2`."""
+    p = randint(2 ** (l - 1), 2**l - 1)
+    p = p - (p % 24) + 23  # make sure p % 24 = 23
+    while not is_safeprime(p):
+        p += 24
+    return p
+
+def random_blumprime(l: int) -> int:
+    """Find a pseudorandom Blum prime with bit length at least `l`."""
+    p = randint(2 ** (l - 1), 2**l - 1)
+    p = p - (p % 4) + 3  # make sure p % 4 = 3
+    while not is_prime(p):
+        p += 4
+    return p
